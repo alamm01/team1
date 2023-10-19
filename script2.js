7633470cd39f12abc97b41a20865d4fe08cb347b
alamm01 <alamm01@yahoo.com>
2023-10-19 13:03:37 -0500

    Added address with hyperlink in the marker

--- script2.js
+++ script2.js
@@ -27,7 +27,7 @@ map.zoomControl.remove();
 L.control.zoom({
   position: 'bottomright'
 }).addTo(map);
-
+// var address
 function geocodeAddress() {
   const address = document.getElementById("address").value;
   if (!address || address.length < 3) {
@@ -51,7 +51,7 @@ function geocodeAddress() {
       }
 
       const foundAddress = featureCollection.features[0];
-      
+      getWeather(foundAddress) 
       test(foundAddress.geometry.coordinates[1], foundAddress.geometry.coordinates[0]);
 
       document.getElementById("name").value = foundAddress.properties.name || '';
@@ -75,30 +75,58 @@ function geocodeAddress() {
 
 function test(lat, long){
 
-  // -87.770231,41.878968,5000
-
   fetch(`https://api.geoapify.com/v2/places?categories=pet&filter=circle:${long},${lat},5000&limit=20&apiKey=ae8fb1509dd84506babaa78221bde1bc`)
     .then(response => response.json())
     .then(result => {console.log(result)
 
-      
     
       for (var i = 0; i<result.features.length; i++){
         marker = L.marker(new L.LatLng(result.features[i].properties.lat, result.features[i].properties.lon), {title:result.features[i].properties.name}).addTo(map)
-        .bindPopup(result.features[i].properties.name);
-      
-      
+        // .bindPopup(result.features[i].properties.name);
+        // var popupContent = result.features[i].properties.name + "\n" + result.features[i].properties.address_line2;
+        // marker.bindPopup(popupContent);
+
+        var name = result.features[i].properties.name;
+        var address = result.features[i].properties.address_line2;
+        var googleMapsLink = "https://www.google.com/maps/search/?api=1&query=" + encodeURIComponent(address);
+
+        var popupContent = name + "<br><a href='" + googleMapsLink + "' target='_blank'>" + address + "</a>";
+
+        marker.bindPopup(popupContent);
+
       };
-    
-    
-    
-    
     })
-
-
-
+    // console.log(result);
     .catch(error => console.log('error', error));
-
-
+    // console.log(result);
 };
 
+function getWeather(foundAddress) {
+  // var apiKey = '3aff5737440b30c8e962f8a23c414db4';
+  // var cityName = 'Minneapolis'
+  // var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${address}&appid=${apiKey}`
+  // // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+  // fetch(requestUrl)
+  //     .then(function (response) {
+  //         return response.json();
+  //     })
+  //     .then(function (data) {
+  //         console.log(data)
+  //         //Loop over the data to generate a table, each table row will have a link to the repo url
+  //         for (var i = 0; i < data.length; i++) {
+  //             // Creating elements, tablerow, tabledata, and anchor
+  //             var createTableRow = document.createElement('tr');
+  //             var tableData = document.createElement('td');
+  //             var link = document.createElement('a');
+
+  //             // Setting the text of link and the href of the link
+  //             link.textContent = data[i].html_url;
+  //             link.href = data[i].html_url;
+
+  //             tableData.appendChild(link);
+  //             createTableRow.appendChild(tableData);
+  //             tableBodyWeather.appendChild(createTableRow);
+  //         }
+  //     });
+}
